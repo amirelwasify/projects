cee80c0fdc5d0cdd683a05df3ee51246b901f5e4
amirelwasify <amiiirr010@gmail.com>
2024-12-14 23:15:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let photo1=document.querySelector('.photo1');
+let photo2=document.querySelector('.photo2');
+photo1.addEventListener('click', function(){
+    alert("هذه صورة احمد السيد زيزو")
+});
+photo2.addEventListener('click', function(){
+    alert("هذه صورة احمد السيد زيزو")
+});
